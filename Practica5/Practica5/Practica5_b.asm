acba7aed6ef2b6f5be39654ff6d01152243428c4
Pedejeca135 <pjco90@hotmail.com>
2020-03-02 14:16:47 -0600

    practica5 b) terminada

--- Practica5/Practica5/Practica5_b.asm
+++ Practica5/Practica5/Practica5_b.asm
@@ -9,6 +9,14 @@ que se presione el push-button de salida, el dato que se elimine de la pila y se
 sea el menor de los datos restantes.
 
  */ 
+
+ .def full_FF = r25 
+ .def menor = r16
+ .def menorL = r23
+ .def menorH = r24 
+ .def consulta = r20 
+
+LDI full_FF, 0xFF
 LDI r17, 0x00 // se carga el registro 17 con 0´s
 STS $0027,r17 // se asigna el valor del registro a una direccion de memoria del registro del puerto c para configurarlos como inputs   Data direction register 
 
@@ -58,14 +66,48 @@ comp:
  JMP pushing // salta
  JMP start // salto a inicio
 
- poping:
+ sta:
+ JMP start
+
+  poping:
  CPI r21,0xFF // comparacion de bandera para aver si la pila esta vacia
  BREQ start // Si sí, salta a inicio 
  ;;; la siguiente linea es la unica que cambia en la
  ;;; implementacion de la fila con respecto al programa a)
- LD r17,Y+ // pone el valor de la direccion y en r17 y baja el valor de la direccion en y
- STS $002B,r17 // muestra en puerto D el valor obtenido del pop
+ LDI menor, 0xFF
+ MOV r30,r28
+ MOV r31,r29
+
+ ciclo_while_poping:
+CP r31, r27 // comparacion si x y Y apuntan a la misma direccion en alto 
+BREQ comp_while // salto si son iguales
+JMP ciclo_while_continue // salto
+comp_while:
+CP r30,r26 // comparacion si x y Y apuntan a la misma direccion en bajo
+BREQ Termina_while // salto si son iguales
+ciclo_while_continue:
+LD consulta,Z
+CP consulta,menor
+//brinca si es menor
+BRLO if_consulta_menor_true
+LD consulta,Z+
+JMP ciclo_while_poping
+
+ Termina_while:
+ MOV r30,menorL
+ MOV r31,menorH
+ MOV r17,menor // pone el valor de la direccion Z en r17 
+ STS $002B,r17//asignar elvalor del registro menor a la salida del puerto D // muestra en puerto D el valor obtenido del pop
+ ST Z,full_FF
  JMP rompe // salto a etiqueta
+
+ //subrutina if(consulta<menor)
+ if_consulta_menor_true:
+ MOV menor,consulta
+ MOV menorH,r31
+ MOV menorL,r30
+ LD consulta,Z+
+ JMP ciclo_while_poping
 
  pushing:
   ST X+,r17 // hace un push a la pila
@@ -75,7 +117,7 @@ comp:
    LDS r19, $0026 // obtiene los valores de entrada en el puerto B
    ANDI r19, 0b0000_1100 // enmascara para solamente ver los bits de push y pop
    CPI r19, 0x00 // Compara si ya se dejaron de aplanar
-   BREQ start // Si sí, salta al inicio  
+   BREQ sta // Si sí, salta al inicio  
    JMP rompe // si no vuelve a llamarse para esperar a que se dejen de apretar
  
 
