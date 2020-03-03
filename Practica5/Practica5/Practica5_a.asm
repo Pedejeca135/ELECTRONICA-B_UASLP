0eca26005fa761d882f0c0f183fd981cbd11c6f1
Pedejeca135 <pjco90@hotmail.com>
2020-03-02 20:17:24 -0600

    codigo comentado, listo para imprimir

--- Practica5/Practica5/Practica5_a.asm
+++ Practica5/Practica5/Practica5_a.asm
@@ -20,10 +20,10 @@ iv. El cuarto push-button termina el programa y apaga todos los leds
  STS $0024,r16 // asignando 0x00 del registro 16 a el registro de direcciones del puerto B(output).
  STS $002A, r16 //Asignando el valor de r16 a la direccion de DDRD($002A) para hacer al Puerto D salidas
 
- LDI r16, 0x00
+ LDI r16, 0x00//entradas en el puerto C (c direccion : 0x27).
  STS $0027, r16
  
- JMP RESET
+ JMP RESET//salto a RESET para empesar todo el proceso.
 
  //MAIN
 START:
@@ -33,25 +33,28 @@ MOV r22,r20//copia registro r20 a r22
  LDS r16, $0026 //$0026Cargamos al registro 16 el valor del puerto C (I/O register).
 
  SBRC r16,0//skip if bit is clear.
- JMP RESET
+ JMP RESET// si es apretado el boton en el pin 0 regresa a RESET.
 
  SBRC r16,1//skip if bit is clear.
- JMP COMPLEMENTO_PUSH
+ JMP COMPLEMENTO_PUSH//si el boton del bit 1 prinde la bandera para el complemento.
 
  SBRC r16,2//skip if bit is clear.
- JMP PAUSE
+ JMP PAUSE//si el boton del bit 2 es apretado de va a la PAUSA.
 
  SBRC r16,3//skip if bit is clear.
- JMP  BEFORE_END
+ JMP  BEFORE_END//si el push button del bit 3 esta en alto se salta a BEFORE_END 
+ //que apaga todos los leds y va a la etiqueta END para terminar el programa.
 
  CPI r24,0x01//checa que este habilitado el complemento.
- BREQ COMPLEMENTO
+ BREQ COMPLEMENTO//si la bandera esta prendida hace el complemento.
  
  STS $0025, r21//pone el primer bit del puerto B.
  STS $002B, r20//pone el puerto D.
 
- DESPUES_DE_ASIGNAR:
+ DESPUES_DE_ASIGNAR:// para indicar en lugar 
+ //donde ya se ha asignado el valor a los puertos de salida.
  
+ //delay de 500MS.
   DELAY_500MS:
   LDI r18,41
   LDI r17,150
@@ -64,29 +67,34 @@ MOV r22,r20//copia registro r20 a r22
 	DEC r18
 	BRNE L1
 	
-
+//compara si ya llego al ultimo bit del puerto D para pasar al puerto B.
  CPI r20,0b1000_0000
- BREQ PAPA_CALIENTE
- LSL r20
+ BREQ PAPA_CALIENTE// pasa el conteo de D a B.
+ LSL r20//recorre el registro r20 a la izquierda.
+ //compara si ya llego al ultimo bit del puerto B para pasar al puerto D.
  CPI r21,0b0010_0000
- BREQ PAPA_CALIENTE_2
- LSL r21
- JMP START
+ BREQ PAPA_CALIENTE_2//pasa el conteo de B a D.
+ LSL r21// recorre el registro r21 a la izquierda.
+ JMP START//si no hay que pasar la papa regresa al start.
+ 
+ //cambia D a B.
  PAPA_CALIENTE:
- LDI r20,0x00
- LDI r21,0x01
- JMP START
- PAPA_CALIENTE_2:
- LDI r20,0x01
- LDI r21,0x00
- JMP START
+ LDI r20,0x00//r20 es para D. Se pone en cero.
+ LDI r21,0x01//r21 es para B. se ingresa un uno.
+ JMP START//regresa al inicio.
 
-  COMPLEMENTO:
- COM r23
- COM r22
+ //cambia B a D.
+ PAPA_CALIENTE_2:
+ LDI r20,0x01//r20 es para D. Se pone en 1 para empezar con el recorrido.
+ LDI r21,0x00//r21 es para B. se ingresa en cero.
+ JMP START//regresa al inicio.
+
+ COMPLEMENTO:
+ COM r23//hace el complemento del registro 23 copiado en el inicio a 21(B).
+ COM r22//hace el complemento del registro 22 copiado en el inicio a 20(D).
  STS $0025, r23//pone el primer bit del puerto B.
  STS $002B, r22//pone el puerto D.
- JMP DESPUES_DE_ASIGNAR
+ JMP DESPUES_DE_ASIGNAR//va a despues de asignar.
 
   ROMPE: 
   LDS r19, $0026 // obtiene los valores de entrada en el puerto C
@@ -107,32 +115,32 @@ CONTINUA_PAUSA:
  JMP CONTINUA_PAUSA
  JMP START
 
- RESET:
- LDI r20, 0x01
- LDI r21, 0x00
+ RESET:// reset para iniciar de nuevo el recorrido.
+ LDI r20, 0x01//inicia r20(D) en uno.
+ LDI r21, 0x00//inicia r21(B) en cero.
  JMP ROMPE
 
- COMPLEMENTO_PUSH:
- CPI r24,0x00
- BREQ SET_COMP
- CPI r24,0x01
- BREQ CLR_COMP
+ COMPLEMENTO_PUSH:// si se aprieta el push button del complemento
+ CPI r24,0x00//si la bandera esta en  cero
+ BREQ SET_COMP// se setea la bandera.
+ CPI r24,0x01// si la bandera esta en uno.
+ BREQ CLR_COMP//se limpia la bandera.
 
- SET_COMP:
+ SET_COMP:// prende la bandera.
  LDI r24, 0x01
- JMP ROMPE
+ JMP ROMPE//control de botones.
 
- CLR_COMP:
+ CLR_COMP:// apaga la bandera.
  LDI r24, 0x00
- JMP ROMPE
+ JMP ROMPE//control de botones.
 
- BEFORE_END:
+ BEFORE_END:// apagar todos los leds.
  LDI r17,0x00
  STS $002B,r17
  STS $0025 ,r17
  JMP END
 
- END:
+ END:/fin del programa.
  JMP END
 
 // SBIS $0010, 4	// Skip if Bit in I/O Register is Set(1) para que siga buscando
